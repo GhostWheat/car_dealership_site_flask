3ff9039b0007753c25d670cd5688785b20abf074
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-04-18 16:03:07 -0500

    still working

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
-
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 # this gives access to the project in ANY OS we are in.
 # allows outside files and folders to be added to the project
 # base directory
 
-# load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -17,9 +17,9 @@ class Config():
     """
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret tunnel"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_ URL') or\
-    'sqlite:///' + os.path.join(basedir,'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_ URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #turns off messages for sqlalchemy
 
 
